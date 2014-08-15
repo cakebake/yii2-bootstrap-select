9f5209fb4adbbbc648f7ea28930a7727a92a9994
Pavel Kilin <github@asdf404.net>
2014-08-15 22:54:19 +0700

    Update BootstrapSelectAsset.php
    Fixed assets path for new bootstrap-select version

--- BootstrapSelectAsset.php
+++ BootstrapSelectAsset.php
@@ -5,13 +5,13 @@ use yii\web\AssetBundle;
 
 class BootstrapSelectAsset extends AssetBundle
 {
-    public $sourcePath = '@vendor/bootstrap-select/bootstrap-select';
+    public $sourcePath = '@vendor/bootstrap-select/bootstrap-select/dist';
     public $css = [
-        'bootstrap-select.min.css',
+        'css/bootstrap-select.min.css',
     ];
-	public $js = [
-		'bootstrap-select.js',
-	];
+    public $js = [
+        'js/bootstrap-select.js',
+    ];
     public $depends = [
         'yii\web\JqueryAsset',
         'yii\bootstrap\BootstrapAsset',
